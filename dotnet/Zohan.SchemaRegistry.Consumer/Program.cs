81ddb0d15418b1493fd41954e7d5d9666fd990f5
David Barkol <dabarkol@microsoft.com>
2020-12-29 15:29:44 -0800

    initial revision

---
+++ dotnet/Zohan.SchemaRegistry.Consumer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zohan.SchemaRegistry.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
